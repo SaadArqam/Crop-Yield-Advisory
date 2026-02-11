cfc6917817c5dac479c76e2b6e0e7f01846443fa
Saad Arqam <saadarqamrnc@gmail.com>
2026-02-11 12:57:25 +0530

    feat: data preprocessing and training

--- ml/inspect_data.py
+++ ml/inspect_data.py
@@ -6,8 +6,11 @@ print("Shape:",df.shape)
 print("Size:",df.size)
 print("\nColumns:")
 print(df.columns)
+# print(df.head())
 
-df = df.drop(columns=['Unnamed: 0'],inplace=True)
+df = df.drop(['Unnamed: 0'], axis=1).reset_index(drop=True)
+print("\nFirst 5 rows:")
+print(df.head())
 
 # X = df.drop(columns=['hg/ha_yield'])
 # y = df['hg/ha_yield']
